98dfbf2e7ac7b50bb7537ce87a530df29502f3e9
samueltanner <samuel.m.s.tanner@gmail.com>
2021-04-26 12:56:33 -0700

    gets login setup

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -10,7 +10,7 @@ class Api::SessionsController < ApplicationController
         Rails.application.credentials.fetch(:secret_key_base), # the secret key
         "HS256" # the encryption algorithm
       )
-      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+      render json: { jwt: jwt, email: user.email, user_id: user.id, username: user.name }, status: :created
     else
       render json: {}, status: :unauthorized
     end
